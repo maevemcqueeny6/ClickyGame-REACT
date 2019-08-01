6cd8edc42d605718dc837801db5b941053ba6fa8
Maeve McQ <maevemcqueeny6@gmail.com>
2019-08-01 19:21:33 -0400

    updated head

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -8,7 +8,7 @@ function Header(count){
         <header style={headerStyle}>
             <h1>Memory Game</h1>
             <h3>Try not to click the same photo twice!</h3>
-            <Link style={linkStyle} to="/">SCOTUS  </Link> | <Link style={linkStyle} to="/FOX">  FOX</Link>
+            <Link style={linkStyle} to="/">Easy  </Link> | <Link style={linkStyle} to="/FOX">Hard</Link>
 
             <h3>Score:  {count.count} </h3>
         </header>
